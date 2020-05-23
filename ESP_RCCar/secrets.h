50365b29d7dd7f718044b1eea1fbbcb4609d6f94
Maciej Anuszkiewicz <maciek.anuszkiewicz@gmail.com>
2020-05-23 19:20:37 +0200

    esp basic sensors

---
+++ ESP_RCCar/secrets.h
@@ -0,0 +1,9 @@
+#define SECRET_SSID ""
+#define SECRET_PASS ""
+#define SECRET_DEVICE_PASSWORD ""
+#define SECRET_MQTT_Broker_server ""
+#define SECRET_MQTT_USER ""
+#define SECRET_MQTT_PASSWORD ""
+#define SECRET_MQTT_CLIENT_NAME ""
+#define SECRET_MQTT_PORT 0
+
